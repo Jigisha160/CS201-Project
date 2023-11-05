1f41e2f8e8bc3fa801e4fde05a9cab209d6016e0
Jigisha160 <134944397+Jigisha160@users.noreply.github.com>
2023-11-05 20:55:22 +0530

    Add files via upload

--- project.cpp
+++ project.cpp
@@ -20,18 +20,6 @@ int minDistance(int dist[], bool sptSet[])
     return min_index;
 }
 
-void printSolution(int dist[][V]) {
-    printf("Following matrix shows the shortest distances between every pair of vertices \n");
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            if (dist[i][j] == INF)
-                printf("%7s", "INF");
-            else
-                printf("%7d", dist[i][j]);
-        }
-        printf("\n");
-    }
-}
 
 vector<vector<int>> Floyd_Warshall(vector<vector<int>> graph) {
     vector<vector<int>> distance(V, vector<int>(V, 0));
